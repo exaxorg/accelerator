e0933e2d0daafc2ec8d425648a3b04edf28b8590
Carl Drougge <bearded@longhaired.org>
2021-12-17 05:33:46 +0100

    + dataset_fanout
    splits a dataset into one for each value of a column.

---
+++ accelerator/standard_methods/a_dataset_fanout.py
@@ -0,0 +1,107 @@
+############################################################################
+#                                                                          #
+# Copyright (c) 2021 Carl Drougge                                          #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#  http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+description = r'''
+Create one dataset per value in a column in a dataset (chain).
+
+Reads source back to previous.source.
+
+All datasets in previous get a dataset here, even if empty.
+'''
+
+from collections import defaultdict
+import re
+
+from accelerator.compat import unicode, izip
+from accelerator import OptionString, NoSuchDatasetError
+from accelerator import subjobs, status
+
+
+options = {
+	'column': OptionString('split based on this column'),
+	'length': -1, # Go back at most this many datasets. You almost always want -1 (which goes until previous.source)
+}
+
+datasets = ('source',)
+
+jobs = ('previous',)
+
+
+def prepare(job):
+	chain = datasets.source.chain(stop_ds={jobs.previous: 'source'}, length=options.length)
+	columns = defaultdict(set)
+	none_support = defaultdict(bool)
+	hashlabel = set()
+	seen_all = set(chain[0].columns)
+	for ds in chain:
+		if options.column not in ds.columns:
+			raise Exception('%r does not have column %r' % (ds, options.column,))
+		hashlabel.add(ds.hashlabel)
+		seen_all &= set(ds.columns)
+		for name, col in ds.columns.items():
+			columns[name].add(col.type)
+			none_support[name] |= col.none_support
+	seen_all.discard(options.column)
+	if not seen_all:
+		raise Exception('Chain has no common columns (except %r)' % (options.column,))
+	columns = {k: columns[k] for k in seen_all}
+	if len(hashlabel) == 1:
+		hashlabel = hashlabel.pop()
+		if hashlabel == options.column:
+			hashlabel = None
+	else:
+		hashlabel = None
+	for name, types in columns.items():
+		if len(types) > 1:
+			raise Exception("Column %r has incompatible types: %r" % (name, types,))
+		columns[name] = (types.pop(), none_support[name],)
+
+	collect = subjobs.build(
+		'dataset_fanout_collect',
+		source=datasets.source,
+		previous=jobs.previous,
+		column=options.column,
+		length=options.length,
+	)
+	values = collect.load()
+	if jobs.previous:
+		previous = {ds.name: ds for ds in jobs.previous.datasets}
+		values.update(previous)
+	else:
+		previous = {}
+
+	with status('Creating %d datasets' % (len(values),)):
+		writers = {
+			name: job.datasetwriter(
+				name=name,
+				columns=columns,
+				hashlabel=hashlabel,
+				previous=previous.get(name),
+				copy_mode=True,
+			)
+			for name in values
+		}
+	return writers, sorted(columns), chain
+
+
+def analysis(sliceno, prepare_res):
+	writers, columns, chain = prepare_res
+	key_it = chain.iterate(sliceno, options.column)
+	values_it = chain.iterate(sliceno, columns, copy_mode=True, status_reporting=False)
+	for key, values in izip(key_it, values_it):
+		writers[unicode(key)].write(*values)
---
+++ accelerator/standard_methods/a_dataset_fanout_collect.py
@@ -0,0 +1,44 @@
+############################################################################
+#                                                                          #
+# Copyright (c) 2021 Carl Drougge                                          #
+#                                                                          #
+# Licensed under the Apache License, Version 2.0 (the "License");          #
+# you may not use this file except in compliance with the License.         #
+# You may obtain a copy of the License at                                  #
+#                                                                          #
+#  http://www.apache.org/licenses/LICENSE-2.0                              #
+#                                                                          #
+# Unless required by applicable law or agreed to in writing, software      #
+# distributed under the License is distributed on an "AS IS" BASIS,        #
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. #
+# See the License for the specific language governing permissions and      #
+# limitations under the License.                                           #
+#                                                                          #
+############################################################################
+
+description = r'''
+Collects values for dataset_fanout.
+
+You probably don't want to call this yourself.
+'''
+
+from accelerator import OptionString
+from accelerator.compat import unicode, imap
+
+
+options = {
+	'column': OptionString,
+	'length': -1,
+}
+
+datasets = ('source',)
+
+jobs = ('previous',)
+
+
+def analysis(sliceno):
+	chain = datasets.source.chain(stop_ds={jobs.previous: 'source'}, length=options.length)
+	return set(imap(unicode, chain.iterate(sliceno, options.column)))
+
+def synthesis(analysis_res):
+	return analysis_res.merge_auto()
