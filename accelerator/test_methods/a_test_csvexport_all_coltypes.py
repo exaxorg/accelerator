346f4d5ad67b0a9488c4ef9b13104d530a65a938
Carl Drougge <bearded@longhaired.org>
2022-07-18 22:20:57 +0200

    [test_csvexport_all_coltypes] remove bits types

--- accelerator/test_methods/a_test_csvexport_all_coltypes.py
+++ accelerator/test_methods/a_test_csvexport_all_coltypes.py
@@ -1,6 +1,6 @@
 ############################################################################
 #                                                                          #
-# Copyright (c) 2020-2021 Carl Drougge                                     #
+# Copyright (c) 2020-2022 Carl Drougge                                     #
 #                                                                          #
 # Licensed under the Apache License, Version 2.0 (the "License");          #
 # you may not use this file except in compliance with the License.         #
@@ -34,8 +34,6 @@ def synthesis(job):
 	dw = job.datasetwriter()
 	todo = {
 		'ascii',
-		'bits32',
-		'bits64',
 		'bool',
 		'bytes',
 		'complex32',
@@ -54,17 +52,16 @@ def synthesis(job):
 	}
 	check = {n for n in _convfuncs if not n.startswith('parsed:')}
 	assert todo == check, 'Missing/extra column types: %r %r' % (check - todo, todo - check,)
-	no_none = {'bits32', 'bits64', 'json'}
 	for name in sorted(todo):
 		if PY2 and name == 'pickle':
 			# pickle columns are not supported on python 2.
 			t = 'ascii'
 		else:
 			t = name
-		dw.add(name, t, none_support=name not in no_none)
+		dw.add(name, t, none_support=True)
 	write = dw.get_split_write()
 	write(
-		'a', 0xffffffff, 0xfedcba9876543210, True, b'hello',
+		'a', True, b'hello',
 		42, 1e100+0.00000000000000001j,
 		date(2020, 6, 23), datetime(2020, 6, 23, 12, 13, 14),
 		1.0, float('-inf'), -10, -20,
@@ -74,7 +71,7 @@ def synthesis(job):
 	d = {}
 	d['recursion'] = d
 	write(
-		'b', 0, 0, False, b'bye',
+		'b', False, b'bye',
 		2-3j, -7,
 		date(1868,  1,  3), datetime(1868,  1,  3, 13, 14, 5),
 		float('inf'), float('nan'), 0, 0,
@@ -82,7 +79,7 @@ def synthesis(job):
 		'...' if PY2 else d, time(13, 14, 5), 'bl\xe4',
 	)
 	write(
-		None, 72, 64, None, None,
+		None, None, None,
 		None, None,
 		None, None,
 		None, None, None, None,
@@ -93,7 +90,7 @@ def synthesis(job):
 	sep = '\x1e'
 	for sep, q, none_as, last_line in (
 		('\x1e', '', None, (
-			'None', '72', '64', 'None', 'None',
+			'None', 'None', 'None',
 			'None', 'None',
 			'None', 'None',
 			'None', 'None', 'None', 'None',
@@ -101,7 +98,7 @@ def synthesis(job):
 			'None', 'None', 'None',
 		)),
 		('\x1e', 'a', '', (
-			'', '72', '64', '', '',
+			'', '', '',
 			'', '',
 			'', '',
 			'', '', '', '',
@@ -109,7 +106,7 @@ def synthesis(job):
 			'', '', '',
 		)),
 		('\x00', '0', None, (
-			'None', '72', '64', 'None', 'None',
+			'None', 'None', 'None',
 			'None', 'None',
 			'None', 'None',
 			'None', 'None', 'None', 'None',
@@ -117,7 +114,7 @@ def synthesis(job):
 			'None', 'None', 'None',
 		)),
 		(':', '"', '"', (
-			'"', '72', '64', '"', '"',
+			'"', '"', '"',
 			'"', '"',
 			'"', '"',
 			'"', '"', '"', '"',
@@ -125,7 +122,7 @@ def synthesis(job):
 			'"', '"', '"',
 		)),
 		(':', '"', {'time': 'never', 'float32': '"0"'}, (
-			'None', '72', '64', 'None', 'None',
+			'None', 'None', 'None',
 			'None', 'None',
 			'None', 'None',
 			'"0"', 'None', 'None', 'None',
@@ -142,7 +139,7 @@ def synthesis(job):
 					assert want == got, 'wanted %r, got %r from %s (export of %s)' % (want, got, exp, ds,)
 				expect(*sorted(todo))
 				expect(
-					'a', '4294967295', '18364758544493064720', 'True', 'hello',
+					'a', 'True', 'hello',
 					'(42+0j)', '(1e+100+1e-17j)',
 					'2020-06-23', '2020-06-23 12:13:14',
 					'1.0', '-inf', '-10', '-20',
@@ -150,7 +147,7 @@ def synthesis(job):
 					'...' if PY2 else '(1+2j)', '12:13:14', 'bl\xe5',
 				)
 				expect(
-					'b', '0', '0', 'False', 'bye',
+					'b', 'False', 'bye',
 					'(2-3j)', '(-7+0j)',
 					'1868-01-03', '1868-01-03 13:14:05',
 					'inf', 'nan', '0', '0',
