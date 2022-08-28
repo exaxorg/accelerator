e024e96007d728dfafb74c6d5c7c6a420650d7dc
Carl Drougge <bearded@longhaired.org>
2022-08-28 13:42:39 +0200

    [dataset_type] abort analysis early when no lines
    regardless of rehashing.
    
    This doesn't really matter, but means the rest of the code no longer
    has to worry about not having any lines (though it might not keep any
    lines after filtering).

--- accelerator/standard_methods/a_dataset_type.py
+++ accelerator/standard_methods/a_dataset_type.py
@@ -295,11 +295,11 @@ def analysis(sliceno, slices, prepare_res):
 			raise Exception("Failed to enable numeric_comma, please install at least one of the following locales: " + " ".join(try_locales))
 		dataset_type.numeric_comma = True
 	dw, dw_bad, dws, lines, chain, column2type, _, rev_rename = prepare_res
+	if chain.lines(sliceno) == 0:
+		dummy = [0] * slices
+		return {}, dummy, {}, {}, dummy
 	if dws:
 		dw = dws[sliceno]
-		if not dw:
-			dummy = [0] * slices
-			return {}, dummy, {}, {}, dummy
 		rehashing = True
 	else:
 		rehashing = False
