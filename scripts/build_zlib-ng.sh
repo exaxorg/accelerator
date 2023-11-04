d36af10f846cc7409c35a5ef565850997450944c
Carl Drougge <bearded@longhaired.org>
2023-11-04 23:56:46 +0100

    [build] update zlib-ng to 2.1.4

--- scripts/build_zlib-ng.sh
+++ scripts/build_zlib-ng.sh
@@ -9,7 +9,7 @@ cd /tmp
 rm -rf zlib-ng
 git clone https://github.com/zlib-ng/zlib-ng.git
 cd zlib-ng
-git checkout cf89cf35037f152ce7adfeca864656de5d33ea1e # 2.1.3
+git checkout 9fb955b8ba734b6519fe1a35704a4bc0ef01a22d # 2.1.4
 CFLAGS="-fPIC -fvisibility=hidden" ./configure --zlib-compat --static --prefix="$ZLIB_PREFIX"
 make install
 cd ..
