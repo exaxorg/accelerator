1ce0e701bb323cb296ffcde502182e06ea30d281
Carl Drougge <bearded@longhaired.org>
2024-03-13 00:46:11 +0100

    [test urd] + a line with build_job set.

--- accelerator/test_methods/a_test_urd.py
+++ accelerator/test_methods/a_test_urd.py
@@ -41,6 +41,7 @@ TEST_LOG_ing = r'''
 3|2023-01-01T00:00:00|add|2023-01|test/ing|{}|[["something", "job-0"]]||it's a caption
 3|2023-02-01T00:00:01|add|2023-02|test/ing|{"test/ing": {"caption": "it's a caption", "joblist": [["something", "job-0"]], "timestamp": "2023-01"}, "test/two": {"caption": "\"caption\\nwithout escaping\"", "joblist": [["something", "job-1"]], "timestamp": "2023-02"}}|[["something", "job-2"]]||
 4	2023-06-19T00:00:00	add	2023-06	test/ing	{}	[["something", "job-3"]]		"caption\nwith lines\nand\ttab\nand \"quotes\" and | too"
+4	2024-03-11T00:00:00	add	2024-03	test/ing	{}	[["something", "job-4"]]		"this one has a build job"	"build_job-0"
 '''.lstrip()
 
 TEST_LOG_two = r'''
@@ -73,18 +74,20 @@ def synthesis(job):
 		assert want == got, '\nWanted %r,\ngot    %r' % (want, got,)
 
 	check('list', ['test/ing', 'test/two'])
-	check('test/ing/since/0', ['2023-01', '2023-02', '2023-06'])
-	check('test/ing/since/2023-01-01', ['2023-02', '2023-06'])
-	check('test/ing/since/2023-02', ['2023-06'])
+	check('test/ing/since/0', ['2023-01', '2023-02', '2023-06', '2024-03'])
+	check('test/ing/since/2023-01-01', ['2023-02', '2023-06', '2024-03'])
+	check('test/ing/since/2023-02', ['2023-06', '2024-03'])
 	check('test/ing/first'  , dict(timestamp='2023-01', user='test', build='ing', build_job=None, joblist=[['something', 'job-0']], caption="it's a caption", deps={}))
 	check('test/ing/2023-01', dict(timestamp='2023-01', user='test', build='ing', build_job=None, joblist=[['something', 'job-0']], caption="it's a caption", deps={}))
 	check('test/two/2023-02', dict(timestamp='2023-02', user='test', build='two', build_job=None, joblist=[['something', 'job-1']], caption=r'"caption\nwithout escaping"', deps={}))
 	check('test/ing/2023-02', dict(timestamp='2023-02', user='test', build='ing', build_job=None, joblist=[['something', 'job-2']], caption='', deps={'test/ing': dict(caption="it's a caption", joblist=[['something', 'job-0']], timestamp='2023-01'), 'test/two': dict(caption=r'"caption\nwithout escaping"', joblist=[['something', 'job-1']], timestamp='2023-02')}))
 	check('test/ing/2023-06', dict(timestamp='2023-06', user='test', build='ing', build_job=None, joblist=[['something', 'job-3']], caption="caption\nwith lines\nand\ttab\nand \"quotes\" and | too", deps={}))
-	check('test/ing/latest' , dict(timestamp='2023-06', user='test', build='ing', build_job=None, joblist=[['something', 'job-3']], caption="caption\nwith lines\nand\ttab\nand \"quotes\" and | too", deps={}))
+	check('test/ing/latest', dict(timestamp='2024-03', user='test', build='ing', build_job='build_job-0', joblist=[['something', 'job-4']], caption="this one has a build job", deps={}))
 	# Test not overriding, with keys in different order and a build_job.
 	check('add', dict(new=False, changed=False, is_ghost=False), b'{"timestamp": "2023-01", "user": "test", "build": "ing", "deps": {}, "joblist": [["something", "job-0"]], "caption": "it\'s a caption", "build_job": "build_job-0"}')
 	check('add', dict(new=False, changed=False, is_ghost=False), b'{"user": "test", "build": "ing", "timestamp": "2023-01", "caption": "it\'s a caption", "deps": {}, "joblist": [["something", "job-0"]], "build_job": "build_job-0"}')
+	# And not overriding, with only build_job being different.
+	check('add', dict(new=False, changed=False, is_ghost=False), b'{"user": "test", "build": "ing", "timestamp": "2024-03", "caption": "this one has a build job", "deps": {}, "joblist": [["something", "job-4"]], "build_job": "changed-0"}')
 	# Changing something should not be allowed.
 	try:
 		check('add', None, b'{"user": "test", "build": "ing", "timestamp": "2023-01", "caption": "new caption", "deps": {}, "joblist": [["something else", "job-0"]], "build_job": "build_job-1"}')
@@ -94,10 +97,10 @@ def synthesis(job):
 	# And now change something with the update flag
 	check('add', dict(new=False, changed=True, is_ghost=False, deps=1), b'{"user": "test", "build": "ing", "timestamp": "2023-01", "caption": "new caption", "deps": {}, "joblist": [["something else", "job-0"]], "flags": ["update"], "build_job": "build_job-1"}')
 	# And that should ghost the 2023-02 entry that depended on the old version
-	check('test/ing/since/0', ['2023-01', '2023-06'])
+	check('test/ing/since/0', ['2023-01', '2023-06', '2024-03'])
 
 	# Test truncation
-	check('truncate/test/ing/2023-02', dict(count=1, deps=0), '')
+	check('truncate/test/ing/2023-02', dict(count=2, deps=0), '')
 	check('test/ing/since/0', ['2023-01'])
 
 	# Try with a user mismatch
