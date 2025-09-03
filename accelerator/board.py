0317ddc576b5d4246d9fe82842033a0635197539
Carl Drougge <bearded@longhaired.org>
2025-09-03 14:14:27 +0200

    [board] don't keep trying to re-import pygments.
    I fixed this mostly to avoid a linter warning.

--- accelerator/board.py
+++ accelerator/board.py
@@ -515,13 +515,10 @@ def run(cfg, from_shell=False, development=False):
 					return template('job_method_list', members=members, job=job)
 			code = tar.extractfile(info).read()
 			bottle.response.content_type = 'text/plain; charset=UTF-8'
-			if info.name.endswith('.py'):
+			if info.name.endswith('.py') and highlight:
 				accept = get_best_accept('text/plain', 'text/html')
 				if accept == 'text/html':
 					try:
-						from pygments import highlight
-						from pygments.lexers import PythonLexer
-						from pygments.formatters import HtmlFormatter
 						data = highlight(code, PythonLexer(), HtmlFormatter())
 						code = '<style>\n' + HtmlFormatter().get_style_defs('.highlight') + '</style>\n' + data
 						bottle.response.content_type = 'text/html; charset=UTF-8'
@@ -735,9 +732,12 @@ def run(cfg, from_shell=False, development=False):
 
 	try:
 		from pygments import highlight
+		from pygments.lexers import PythonLexer
+		from pygments.formatters import HtmlFormatter
 	except Exception:
 		from accelerator.colour import bold, red, green
 		print(bold("\nINFO: Install \"pygments\" to have " + red("prett") + green("ified") + " source code in board.\n"))
+		highlight = None
 
 	bottle.TEMPLATE_PATH = [os.path.join(os.path.dirname(__file__), 'board')]
 	kw = {}
